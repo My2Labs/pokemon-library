c399ddcc8487c633508fdd9daa5237d2f3a22e30
Sharron <sking171@ford.com>
2022-04-01 08:26:10 -0500

    Initial commit.

---
+++ site/index.js
@@ -0,0 +1,21 @@
+const app = document.querySelector(".pokemon")
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=73"
+
+function addPokeImage(url) {
+    const img = document.createElement("img")
+    img.src = url
+    app.append(img)
+}
+
+fetch(url)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
+    }).then(responses => {
+        responses.forEach(response => {
+            addPokeImage(response.sprites.front_shiny)
+        })
+    })
