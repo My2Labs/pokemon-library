ce6ef342cafc3a55056201fbc0ba238bd38edd1f
Sharron <sking171@ford.com>
2022-04-04 07:16:06 -0500

    Add main page code.

--- site/index.js
+++ site/index.js
@@ -1,21 +1,42 @@
 const app = document.querySelector(".pokemon")
-const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=73"
+const div = document.createElement("div")
 
-function addPokeImage(url) {
-    const img = document.createElement("img")
-    img.src = url
-    app.append(img)
-}
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=73"
+const a = document.createElement("a")
+const ul = document.querySelector("ul")
+const main = document.querySelector("main")
+ul.append(a)
+const p = document.createElement("p")
+
+
 
 fetch(url)
-    .then(response => {
-        return response.json()
-    }).then(parsedResponse => {
-        const urls = parsedResponse.results.map(result => result.url)
-        const fetches = urls.map(url => fetch(url).then(response => response.json()))
-        return Promise.all(fetches)
-    }).then(responses => {
-        responses.forEach(response => {
-            addPokeImage(response.sprites.front_shiny)
+    .then(response => response.json())
+    .then((response) => {
+        const sharronsPokemon = response.results
+        const myList = sharronsPokemon
+            .map(pokemon => pokemon.url)
+            .map(url => {
+                return fetch(url).then(response => response.json())
+            })
+        console.log(myList)
+        return Promise.all(myList)
+    }).then(whatwasreturned => {
+        whatwasreturned.map(response => {
+            const fiftyPokemon = document.createElement("button")
+            fiftyPokemon.classList = "fifty-pokemon"
+            const name = `${response.species.name[0].toUpperCase()}${response.species.name.slice(1)}`;
+            fiftyPokemon.innerHTML = `
+                            <p>
+                                <img src="${response.sprites.front_shiny}" alt="${name}" />
+                                <figcaption><a href="pokemon.html?pokemon=${response.id}">${name}</a></figcaption>
+                            </p>
+                        `
+            console.log(fiftyPokemon)
+            return fiftyPokemon
+        }).forEach(pokeListing => {
+            const spinner = document.querySelector(".spinner")
+            spinner.classList.add("hidden")
+            ul.append(pokeListing)
         })
     })
