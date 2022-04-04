f32db0bacc5d36bcba9933f35b4ac3f348c93c08
Sharron <sking171@ford.com>
2022-04-04 13:26:15 -0500

    Add pokemon.js and pokemon.html.

--- site/index.js
+++ site/index.js
@@ -1,6 +1,6 @@
 const app = document.querySelector(".pokemon")
 const div = document.createElement("div")
-const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=73"
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=173"
 const a = document.createElement("a")
 const ul = document.querySelector("ul")
 const main = document.querySelector("main")
@@ -28,7 +28,7 @@ fetch(url)
             fiftyPokemon.innerHTML = `
                             <p>
                                 <img src="${response.sprites.front_shiny}" alt="${name}" />
-                                <figcaption><a href="pokemon.html?pokemon=${response.id}">${name}</a></figcaption>
+                                <p><a href="pokemon.html?/pokemon=${response.abilities}${response.height}${response.weight}">${name}</a></p>
                             </p>
                         `
             console.log(fiftyPokemon)
@@ -39,33 +39,3 @@ fetch(url)
             ul.append(pokemonList)
         })
     })
-
-//THIS CODE ONLY SHOWS ONE POKEMON AT A TIME. 
-// const pokeData = {
-//     url: `https://pokeapi.co/api/v2/`,
-//     type: `pokemon`,
-//     ability: `5`,
-// }
-
-// li.append(pokeData)
-
-// const { url, type, ability } = pokeData
-
-// const pokeUrl = `${pokeData.url}${pokeData.type}/${pokeData.ability}`
-
-// fetch(pokeUrl)
-//     .then((response) => (response.json()))
-//     .then((pokeShow) => showPoke(pokeShow))
-
-// const showPoke = (response) => {
-//     console.log(response)
-//     const html = `
-//         <li class="name">
-//         <div class="pokeName">${response.name}</div>
-//         <img src=${response.sprites.front_default}>
-//         <div class="details">Height: ${response.height}
-//         Weight: ${response.weight}</div>
-//         </li>
-//         `
-//     const pokemonDiv = document.querySelector(".pokemon")
-//     pokemonDiv.innerHTML = html
