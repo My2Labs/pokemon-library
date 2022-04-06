6aa47cf354e38fca2b34cd3afd7dc1049418ae66
Sharron <sking171@ford.com>
2022-04-06 07:21:31 -0500

    Make corrections.

--- site/index.js
+++ site/index.js
@@ -5,32 +5,28 @@ const ul = document.querySelector("ul")
 ul.append(a)
 const p = document.createElement("p")
 
-
-
-
 fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=173")
     .then(response => response.json())
     .then((response) => {
-        const sharronsPokemon = response.results
-        const myList = sharronsPokemon
+        const pokemonLists = response.results
+        const pokemonList = pokemonLists
             .map(pokemon => pokemon.url)
             .map(url => {
                 return fetch(url).then(response => response.json())
             })
 
-        return Promise.all(myList)
+        return Promise.all(pokemonList)
     }).then(responses => {
         responses.map(response => {
-            const fiftyPokemon = document.createElement("button")
-            fiftyPokemon.classList = "pokemon-listing"
+            const pokemonButtons = document.createElement("button")
             const name = `${response.species.name[0].toUpperCase()}${response.species.name.slice(1)}`;
-            fiftyPokemon.innerHTML = `
+            pokemonButtons.innerHTML = `
                 <p>
                     <img src="${response.sprites.front_shiny}" alt="${name}" />
                     <p><a href="pokemon.html?pokemon=${response.id}">${name}</a></p>
                 </p>
             `
-            return fiftyPokemon
+            return pokemonButtons
         }).forEach(pokeListing => {
             const spinner = document.querySelector(".spinner")
             spinner.classList.add("hidden")
